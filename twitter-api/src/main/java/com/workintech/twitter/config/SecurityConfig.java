2c6cb673230d9931b4a82c8051fb438c40b4f729
Tayfun <tayfun.software96@gmail.com>
2026-02-26 00:47:55 +0300

    add Dockerfile for Render deploy, vercel.json, CORS and application.properties config

--- twitter-api/src/main/java/com/workintech/twitter/config/SecurityConfig.java
+++ twitter-api/src/main/java/com/workintech/twitter/config/SecurityConfig.java
@@ -53,7 +53,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3200"));
+        configuration.setAllowedOrigins(List.of(
+                "https://twitter-app-topaz.vercel.app",
+                "http://localhost:3200"
+                ));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
